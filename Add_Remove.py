4c5859adde98377759e1a9339954114d058935da
Berk Efe Keskin <92454444+AkioKiyota@users.noreply.github.com>
2022-05-04 20:39:07 +0300

    Update Add_Remove.py

--- Add_Remove.py
+++ Add_Remove.py
@@ -1,27 +1,27 @@
 import os
 
 def add(mov):
-    with open(f"Cinema/{mov}.txt", "w") as db:
+    with open(f"{mov}.txt", "w") as db:
         db.write(mov + ":\n")
         for x in range(1,41):
             db.write("empty\n")
             
 def remove(mov):
-    os.remove(f"Cinema/{mov}.txt")
+    os.remove(f"{mov}.txt")
             
 
 choice = input("Add or remove movie. [Add/Remove]\n")
 
 if choice == "Add" or choice == "add" :
     q = input("Enter the movie name:\n")
-    exists = os.path.exists(f"Cinema/{q.strip}.txt")
+    exists = os.path.exists(f"{q.strip}.txt")
     if exists:
         print("This movie already exists!")
     elif  not exists:
         add(q.strip())
 elif choice == "Remove" or choice == "remove" :
     w = input("Enter the movie name:\n")
-    exists = os.path.exists(f"Cinema/{w.strip()}.txt")
+    exists = os.path.exists(f"{w.strip()}.txt")
     if exists:
         remove(w.strip())
     elif not exists:
